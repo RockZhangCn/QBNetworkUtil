361fa0a87dc2b92882ec2e9433dc73ef3420aced
Rock <rock.zhang.cn@gmail.com>
2016-02-16 17:27:46 +0800

    Initial create the project with AS contains only a single fullscreen activity

---
+++ app/src/androidTest/java/tencent/com/rocksnzhang/qbnetworkutil/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tencent.com.rocksnzhang.qbnetworkutil;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
