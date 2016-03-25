659394b182a63ef74be6618a3eb4701ce9750d46
Rock <rock.zhang.cn@gmail.com>
2016-03-25 17:36:01 +0800

    DNS add HttpDNS fine

--- app/src/main/java/com/tencent/rocksnzhang/nettools/DNSResolver.java
+++ app/src/main/java/com/tencent/rocksnzhang/nettools/DNSResolver.java
@@ -1,9 +1,20 @@
 package com.tencent.rocksnzhang.nettools;
 
+import android.util.Log;
+
 import com.tencent.rocksnzhang.utils.DetectResultListener;
 import com.tencent.rocksnzhang.utils.DetectTask;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.URL;
+import java.util.Enumeration;
 
 /**
  * Created by rock on 16-2-25.
@@ -17,6 +28,12 @@ public class DNSResolver extends DetectTask
 
     @Override
     public void taskRun()
+    {
+        //nativeDnsResolve();
+        HttpDnsResolve();
+    }
+
+    private void nativeDnsResolve()
     {
         StringBuilder sb = new StringBuilder();
         try
@@ -35,6 +52,35 @@ public class DNSResolver extends DetectTask
         {
             finishedTask(false, e.toString());
         }
+    }
+
+    private void HttpDnsResolve()
+    {
+            final String serverUrl = "http://182.254.116.116/d?dn=";
+            String htmlCode = "";
+            try {
+                InputStream in;
+                URL url = new java.net.URL(serverUrl + mHost );
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+                connection.connect();
+
+                in = connection.getInputStream();
+                htmlCode = IOUtils.toString(in, "UTF-8");
+                Log.e("ROCK", serverUrl + mHost  + " ==> " + htmlCode);
+            } catch (Exception e)
+            {
+                finishedTask(false, e.toString());
+                return;
+            }
+
+            if (htmlCode == null || htmlCode.equals("")) {
+                finishedTask(false, "empty content");
+                return ;
+            }
+
+            finishedTask(true, htmlCode);
     }
 
     @Override
